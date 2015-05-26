a33d7b4e3aeeef8f3f2a6f01d79c2f1512810a97
Incognito-legend <cameron_wilson@student.sscps.org>
2015-05-26 09:13:13 -0400

    didnt do much today

--- Objective_Arrow.java
+++ Objective_Arrow.java
@@ -8,31 +8,37 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Objective_Arrow extends Actor
 {
-    GreenfootImage Objective_Arrow_1 = new GreenfootImage("Objective_Arrow_1(up).png");
-    GreenfootImage Objective_Arrow_2 = new GreenfootImage("Objective_Arrow_2(up).png");
     int counter = 0;
     int timer = 0;
-    int width = 216;
-    int height = 256;
+    int time_count = 0;
     public void act() 
     {
             int width = 216;
             int height = 256;
+
                if (counter == 0 && timer == 9) {
             setImage("Objective_Arrow_1(up).png");
 //             setBackground(map_Lobby);
+            System.out.println("time_count = " + Integer.toString(timer));
         } else if (counter == 1 && timer == 9) {
             setImage("Objective_Arrow_1(up).png");
+            
+            System.out.println("time_count = " + Integer.toString(timer));
         } else if (counter == 2 && timer == 9) {
             setImage("Objective_Arrow_1(up).png");
+            System.out.println("time_count = " + Integer.toString(timer));
         } else if (counter == 3 && timer == 9) {
             setImage("Objective_Arrow_2(up).png");
+            System.out.println("time_count = " + Integer.toString(timer));
         } else if (counter == 4 && timer == 9) {
             setImage("Objective_Arrow_2(up).png");
+            System.out.println("time_count = " + Integer.toString(timer));
         } else if (counter == 5 && timer == 9) {
             setImage("Objective_Arrow_2(up).png");
+            System.out.println("time_count = " + Integer.toString(timer));
         }
+//         time_count = time_count + 1;
         counter = (counter + 1) % 6;
-        timer = (timer +1) % 10;
+        timer = (timer + 1) % 10;
         }
     }    
