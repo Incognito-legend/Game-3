2f84effd2f2893050d168499d8b60bbe9e6faa62
Incognito-legend <cameron_wilson@student.sscps.org>
2015-05-27 13:55:36 -0400

    added in timer

--- Objective_Arrow.java
+++ Objective_Arrow.java
@@ -9,36 +9,33 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Objective_Arrow extends Actor
 {
     int counter = 0;
-    int timer = 0;
-    int time_count = 0;
+    private long lastMark = System.currentTimeMillis();
+    private SimpleTimer timer = new SimpleTimer();
+//     int timer = 0;
+//     int seconds = 0;
     public void act() 
     {
             int width = 216;
             int height = 256;
-
-               if (counter == 0 && timer == 9) {
-            setImage("Objective_Arrow_1(up).png");
-//             setBackground(map_Lobby);
-            System.out.println("time_count = " + Integer.toString(timer));
-        } else if (counter == 1 && timer == 9) {
-            setImage("Objective_Arrow_1(up).png");
+if ((counter == 0) && (timer.millisElapsed() > 1000 && Gr           eenfoot.isKeyDown("space"))){
+     setImage("Objective_Arrow_1(up).png");
+timer.mark();
+             System.out.println("seconds = " + Integer.toString(timer.millisElapsed()));
+        } else if ((counter == 1) && (timer.millisElapsed() > 1000 && Greenfoot.isKeyDown("space"))){
             
-            System.out.println("time_count = " + Integer.toString(timer));
-        } else if (counter == 2 && timer == 9) {
-            setImage("Objective_Arrow_1(up).png");
-            System.out.println("time_count = " + Integer.toString(timer));
-        } else if (counter == 3 && timer == 9) {
             setImage("Objective_Arrow_2(up).png");
-            System.out.println("time_count = " + Integer.toString(timer));
-        } else if (counter == 4 && timer == 9) {
-            setImage("Objective_Arrow_2(up).png");
-            System.out.println("time_count = " + Integer.toString(timer));
-        } else if (counter == 5 && timer == 9) {
-            setImage("Objective_Arrow_2(up).png");
-            System.out.println("time_count = " + Integer.toString(timer));
+timer.mark();
+             System.out.println("seconds = " + Integer.toString(timer.millisElapsed()));
+
         }
-//         time_count = time_count + 1;
-        counter = (counter + 1) % 6;
-        timer = (timer + 1) % 10;
+        counter = (counter + 1) % 2;
         }
+    public void mark()
+    {
+        lastMark = System.currentTimeMillis();
+    }
+        public int millisElapsed()
+    {
+        return (int) (System.currentTimeMillis() - lastMark);
+    }
     }    
